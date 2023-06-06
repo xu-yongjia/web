022a79dee6cbbaeea5b332a851eda021b9589d40
xu-yongjia <2359865877@qq.com>
2023-06-07 00:16:11 +0800

    更新 AssignDelivery.go

--- GIN/API_back/AssignDelivery.go
+++ GIN/API_back/AssignDelivery.go
@@ -2,6 +2,7 @@ package api2
 
 import (
 	"gintest/DBstruct"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,7 +14,7 @@ type AssignDeliveryRequest struct {
 func AssignDelivery(c *gin.Context) {
 	var assignDeliveryRequest AssignDeliveryRequest
 	if err := c.ShouldBindJSON(&assignDeliveryRequest); err != nil {
-		c.JSON(400, gin.H{"msg": "数据格式错误", "status": 400})
+		c.JSON(400, gin.H{"msg": "数据格式错误", "status": 201})
 		return
 	}
 
@@ -21,7 +22,7 @@ func AssignDelivery(c *gin.Context) {
 	result := DBstruct.DB.First(&delivery, assignDeliveryRequest.DeliverID)
 
 	if result.Error != nil {
-		c.JSON(400, gin.H{"msg": "无法找到指定的配送员", "status": 400})
+		c.JSON(201, gin.H{"msg": "无法找到指定的配送员", "status": 201})
 		return
 	}
 
@@ -29,12 +30,12 @@ func AssignDelivery(c *gin.Context) {
 		var order DBstruct.Order
 		result := DBstruct.DB.First(&order, orderID)
 		if result.Error != nil {
-			c.JSON(400, gin.H{"msg": "无法找到指定的订单", "status": 400})
+			c.JSON(201, gin.H{"msg": "无法找到指定的订单", "status": 201})
 			return
 		}
 
 		if order.Status != "已支付" {
-			c.JSON(400, gin.H{"msg": "订单状态必须为'已支付'", "status": 400})
+			c.JSON(201, gin.H{"msg": "订单状态必须为'已支付'", "status": 201})
 			return
 		}
 
