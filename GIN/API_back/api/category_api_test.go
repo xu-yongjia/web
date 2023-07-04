e84ef11727aed985fc4e3c781e6b9528ad7ba5d4
xu-yongjia <2359865877@qq.com>
2023-07-04 10:52:50 +0800

    添加注释

--- GIN/API_back/api/category_api_test.go
+++ GIN/API_back/api/category_api_test.go
@@ -18,10 +18,13 @@ func handleError(t *testing.T, err error) {
 	}
 }
 func server() {
+	//关闭gin的日志输出
 	gin.SetMode(gin.ReleaseMode)
-	//gin.DefaultWriter = ioutil.Discard
+	//数据库的日志输出在DBStruct/init.go中使用语句db.LogMode()配置，已改为false
+	//打开数据库
 	dsn := "root:123456@tcp(127.0.0.1:3306)/testdb?charset=utf8mb4&parseTime=True&loc=Local"
 	DBstruct.Database(dsn)
+	//在这里配置一个路由器并运行，配置的路由器时不加token中间件，这样发送的请求不需要带token
 	r := gin.Default()
 	v1 := r.Group("/api/")
 	v1.POST("GetCategoryList", GetCategoryList)
@@ -33,20 +36,21 @@ func server() {
 }
 
 func TestGetCategoryList(t *testing.T) {
+	//启动一个专用于测试的服务器
 	go server()
 
 	url := "http://127.0.0.1:3000/api/GetCategoryList"
-
-	data := make(map[string]interface{})
-	data["canteen_id"] = 1
-	bytesData, _ := json.Marshal(data)
-	reqest, _ := http.NewRequest("POST", url, bytes.NewReader(bytesData))
-	reqest.Header.Add("Content-Type", "application/json")
-
+	//配置请求内容
+	data := make(map[string]interface{})                                  //创建一个map类型，从字符串映射到任意类型，作为请求的头部
+	data["canteen_id"] = 1                                                //配置data的内容
+	bytesData, _ := json.Marshal(data)                                    //将data编码为json格式，byte[]类型
+	reqest, _ := http.NewRequest("POST", url, bytes.NewReader(bytesData)) //用url和bytesData创建一个请求结构体
+	reqest.Header.Add("Content-Type", "application/json")                 //设置头部字段Content-Type为json格式
+	//发送请求，获取返回数据
 	client := &http.Client{}
 	resp, _ := client.Do(reqest)
 	body, _ := ioutil.ReadAll(resp.Body)
-
+	//格式化返回的json数据并打印到日志
 	var str bytes.Buffer
 	_ = json.Indent(&str, []byte(body), "", "    ")
 	t.Log(str.String())
