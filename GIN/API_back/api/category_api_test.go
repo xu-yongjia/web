a111e70a1caccde285152d5598c8d591342a5313
wujf37 <2945813296@qq.com>
2023-07-04 13:52:23 +0800

    category_test 更新

--- GIN/API_back/api/category_api_test.go
+++ GIN/API_back/api/category_api_test.go
@@ -55,3 +55,89 @@ func TestGetCategoryList(t *testing.T) {
 	_ = json.Indent(&str, []byte(body), "", "    ")
 	t.Log(str.String())
 }
+
+func TestAddCategory(t *testing.T) {
+	//启动一个专用于测试的服务器
+	go server()
+
+	url := "http://127.0.0.1:3000/api/AddCategory"
+	//配置请求内容
+	data := make(map[string]interface{}) //创建一个map类型，从字符串映射到任意类型，作为请求的头部
+	data["canteen_id"] = 1
+	data["category_name"] = "烧卤"                                          //配置data的内容
+	bytesData, _ := json.Marshal(data)                                    //将data编码为json格式，byte[]类型
+	reqest, _ := http.NewRequest("POST", url, bytes.NewReader(bytesData)) //用url和bytesData创建一个请求结构体
+	reqest.Header.Add("Content-Type", "application/json")                 //设置头部字段Content-Type为json格式
+	//发送请求，获取返回数据
+	client := &http.Client{}
+	resp, _ := client.Do(reqest)
+	body, _ := ioutil.ReadAll(resp.Body)
+	//格式化返回的json数据并打印到日志
+	var str bytes.Buffer
+	_ = json.Indent(&str, []byte(body), "", "    ")
+	t.Log(str.String())
+}
+
+func TestUpdateCategory(t *testing.T) {
+	//启动一个专用于测试的服务器
+	go server()
+
+	url := "http://127.0.0.1:3000/api/UpdateCategory"
+	//配置请求内容
+	data := make(map[string]interface{}) //创建一个map类型，从字符串映射到任意类型，作为请求的头部
+	data["category_id"] = 1
+	data["category_name"] = "自选"                                          //配置data的内容
+	bytesData, _ := json.Marshal(data)                                    //将data编码为json格式，byte[]类型
+	reqest, _ := http.NewRequest("POST", url, bytes.NewReader(bytesData)) //用url和bytesData创建一个请求结构体
+	reqest.Header.Add("Content-Type", "application/json")                 //设置头部字段Content-Type为json格式
+	//发送请求，获取返回数据
+	client := &http.Client{}
+	resp, _ := client.Do(reqest)
+	body, _ := ioutil.ReadAll(resp.Body)
+	//格式化返回的json数据并打印到日志
+	var str bytes.Buffer
+	_ = json.Indent(&str, []byte(body), "", "    ")
+	t.Log(str.String())
+}
+
+func TestHasDelCategory(t *testing.T) {
+	//启动一个专用于测试的服务器
+	go server()
+
+	url := "http://127.0.0.1:3000/api/HasDelCategory"
+	//配置请求内容
+	data := make(map[string]interface{})                                  //创建一个map类型，从字符串映射到任意类型，作为请求的头部
+	data["category_id"] = 123                                             //配置data的内容
+	bytesData, _ := json.Marshal(data)                                    //将data编码为json格式，byte[]类型
+	reqest, _ := http.NewRequest("POST", url, bytes.NewReader(bytesData)) //用url和bytesData创建一个请求结构体
+	reqest.Header.Add("Content-Type", "application/json")                 //设置头部字段Content-Type为json格式
+	//发送请求，获取返回数据
+	client := &http.Client{}
+	resp, _ := client.Do(reqest)
+	body, _ := ioutil.ReadAll(resp.Body)
+	//格式化返回的json数据并打印到日志
+	var str bytes.Buffer
+	_ = json.Indent(&str, []byte(body), "", "    ")
+	t.Log(str.String())
+}
+
+func TestDelCategory(t *testing.T) {
+	//启动一个专用于测试的服务器
+	go server()
+
+	url := "http://127.0.0.1:3000/api/DelCategory"
+	//配置请求内容
+	data := make(map[string]interface{})                                  //创建一个map类型，从字符串映射到任意类型，作为请求的头部
+	data["category_id"] = 123                                             //配置data的内容
+	bytesData, _ := json.Marshal(data)                                    //将data编码为json格式，byte[]类型
+	reqest, _ := http.NewRequest("POST", url, bytes.NewReader(bytesData)) //用url和bytesData创建一个请求结构体
+	reqest.Header.Add("Content-Type", "application/json")                 //设置头部字段Content-Type为json格式
+	//发送请求，获取返回数据
+	client := &http.Client{}
+	resp, _ := client.Do(reqest)
+	body, _ := ioutil.ReadAll(resp.Body)
+	//格式化返回的json数据并打印到日志
+	var str bytes.Buffer
+	_ = json.Indent(&str, []byte(body), "", "    ")
+	t.Log(str.String())
+}
