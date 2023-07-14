22a39911e65cd743a14db560ca83ba379b812acb
xu-yongjia <2359865877@qq.com>
2023-07-15 01:48:58 +0800

    README

--- orderingSystem/vue.config.js
+++ orderingSystem/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     open: true,
     proxy: {
       '/api': {
-        target: ' http://3f5778a9.r3.cpolar.top',
+        target: ' http://2eb7d14.r3.cpolar.top',
         changeOrigin: true, //允许跨域
         pathRewrite: {
           '^/api': '/api/v1'
